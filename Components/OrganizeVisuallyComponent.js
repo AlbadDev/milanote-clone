42ed777ac19cd44f9e094ab763dec80f85432815
AlbadDev <alioubkeita95@gmail.com>
2022-04-27 00:25:05 +0000

    Update: add header title and paragraph with design styled

--- Components/OrganizeVisuallyComponent.js
+++ Components/OrganizeVisuallyComponent.js
@@ -10,6 +10,9 @@ const OrganizeVisuallyComponent = () => {
             <article className={style.articleContainer}>
                 <div className={style.header}>
                     <h2 className={style.title}>Organize visually</h2>
+                    <p className={style.paragraph}>
+                    Milanote s flexible drag and drop interface lets you arrange things in whatever way makes sense for your project.
+                    </p>
                 </div>
                 <div className={style.body}>bla bla bla</div>
             </article>
