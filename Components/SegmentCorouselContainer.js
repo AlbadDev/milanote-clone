746280f205b0e4704233423a99f5b5fcb5f59aa9
Albaddev <alioubkeita95@gmail.com>
2022-05-07 00:30:15 +0000

    Update: setup and style fake mock container

--- Components/SegmentCorouselContainer.js
+++ Components/SegmentCorouselContainer.js
@@ -9,6 +9,12 @@ const SegmentCorouselContainer = () => {
 
     return (
         <section className={style.sectionContainer}>
+            <header className={style.header}>
+                <h3 className={style.title}>Designers use Milanote to organize their projects</h3>
+            </header>
+            <div className={style.buttonContainer}>
+                <button className={style.findMe}>Find out how</button>
+            </div>
             <article className={style.articleContainer}>
                 Hello Im A Container
             </article>
