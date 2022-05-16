fe8a767bac56fcbbbfaf7d72aec4bfe2a8ea2414
albaddev <alioubkeita95@gmail.com>
2022-05-16 14:59:41 +0000

    New: initialize json data for the corousel Component

---
+++ dataJson.js
@@ -0,0 +1,41 @@
+
+export const corouselData = [
+	{	
+		id: '0',
+		title: 'Designers use Milanote to organize their projects',
+		linkTo: 'https://milanote.com/inspiration/ux-designers',
+		imgContainer:'https://images.prismic.io/milanote/281e769e-a20f-4baa-8966-e95bf03ea3f9_Carousel-designers+2021+-+November.png'
+	},
+	{
+		id: '1',
+		title: 'Creative directors use Milanote to organize their inspiration into moodboards',
+		linkTo: 'https://milanote.com/inspiration/creative-directors',
+		imgContainer: 'https://images.prismic.io/milanote/6f00180b-62d7-47cf-ac0a-0502fdeb5ba8_Carousel-creativedirectors+2021+-+November.png'
+	},
+	{
+		id: '2',
+		title: 'Marketers use Milanote to organize their campaigns',
+		linkTo: 'https://milanote.com/inspiration/marketers',
+		imgContainer: 'https://images.prismic.io/milanote/bdf7c8db-425b-49ed-8a44-534ba2a35005_Carousel-marketers+2021+-+November.png'
+	},
+	{	
+		id: '3',
+		title: 'Writers use Milanote to outline their stories',
+		linkTo: 'https://milanote.com/product/writing-software',
+		imgContainer: 'https://images.prismic.io/milanote/b9230cac-4385-4191-8eb1-22a2c9ca2099_Carousel-writers+2021+-+November.png'
+	},
+	{
+		id: '4',
+		title: 'Agencies use Milanote to organize their creative projects',
+		linkTo: 'https://milanote.com/inspiration/agencies',
+		imgContainer: 'https://images.prismic.io/milanote/6653f0b8-7e52-4171-8a28-17c6c617bbc9_Carousel-agencies+2021+-+November.png'
+	},
+	{
+		id: '5',
+		title: 'Startups use Milanote to figure out what's next',
+		linkTo: 'https://milanote.com/inspiration/startups',
+		imgContainer: 'https://images.prismic.io/milanote/171ca694-65fd-4d42-a8dd-eaf6b7e97e87_Carousel-startups+2021+-+November.png'
+	}
+
+
+]
