8fa5558f67199f20cfdfb5dfd76a96ddf393a9a7
albaddev <alioubkeita95@gmail.com>
2022-05-16 15:18:59 +0000

    New: setup api page/index request handler

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { corouselData } from "../../dataJson"
+
+
+
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(corouselData)
 }
