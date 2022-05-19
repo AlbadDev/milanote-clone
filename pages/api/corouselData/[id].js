f60a7000d926906b0033f0c01de6c2d42db42d5d
albaddev <alioubkeita95@gmail.com>
2022-05-19 16:59:44 +0000

    Update: delete some line of text

--- pages/api/corouselData/[id].js
+++ pages/api/corouselData/[id].js
@@ -1,35 +1,18 @@
-<<<<<<< HEAD
+
 import { corouselData } from "../../../dataJson";
-=======
-import { corouselData } from "../../../dataJson"
 
 
 
->>>>>>> 1a539a62dbf887aff4f203a38eed234c9a730229
 
-
-
-const handler = (req, res) => {
-<<<<<<< HEAD
-    const filterCorousel = corouselData.filter(doc => doc.id === req.query.id)
-
-
-    if(filterCorousel.length > 0) {
-        res.status(200).json(filterCorousel)
-    } else {
-        res.status(404).json(filterCorousel[0])
-    }
-}
-
-=======
+export default function handler (req, res) {
     const filteredCorouselData =  corouselData.filter(corData => corData.id === req.query.id)
 
-        if(filteredCorouselData.lenght > 0) {
-            res.status(200).json(filteredCorouselData)
-         } else {
-           res.status(404).json(filteredCorouselData[0])
-        }
-        
+    if (filteredCorouselData.lenght > 0)  res.status(200).json(filteredCorouselData)
+    
+    else res.status(404).json(filteredCorouselData[0])
+    
   }
->>>>>>> 1a539a62dbf887aff4f203a38eed234c9a730229
-export default handler
+    
+
+        
+   
--- pages/api/corouselData/index.js
+++ pages/api/corouselData/index.js
@@ -4,10 +4,6 @@ import { corouselData } from "../../../dataJson"
 
 
 
-<<<<<<< HEAD
 export default function handler (req, res) {
-=======
-export default function handler(req, res) {
->>>>>>> 1a539a62dbf887aff4f203a38eed234c9a730229
   res.status(200).json(corouselData)
 }
