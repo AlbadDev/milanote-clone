d84ec5155604e13e4bc53591ed9618195e131890
Albaddev <alioubkeita95@gmail.com>
2022-05-21 00:36:16 +0000

    Update: delete yarn lock file

--- Components/SegmentCorouselSlice.js
+++ Components/SegmentCorouselSlice.js
@@ -1,6 +1,6 @@
 import style from '../styles/SegmentCorouselSlice.module.scss'
 import SegmentCorouselContainer from './SegmentCorouselContainer'
-
+import Link from 'next/link'
 
 
 
@@ -15,7 +15,7 @@ const SegmentCorouselSlice = () => {
             <div className={style.navContainer}>
                 <span className={style.usage}>Milanote is used by:</span>
                 <ul>
-                    <li>Designers</li>
+                    <li><Link href='http://localhost:3000/api/corouselData/3'>Designers</Link></li>
                     <li>Creative directors</li>
                     <li>Marketers</li>
                     <li>Writers</li>
